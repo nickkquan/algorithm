43e34061313f35b84cd8360a406bb4c8fab4b6b0
Nick Quan <nickkquan@gmail.com>
2018-05-15 14:51:26 -0700

    reverse string complete

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,19 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+	// Using higher order functions
+	return str
+		.split("")
+		.reverse()
+		.join("");
+
+	// Using a for loop
+	var result = "";
+	for (var index = str.length - 1; index >= 0; index--) {
+		result += str[index];
+	}
+	return result;
+}
 
 module.exports = reverse;

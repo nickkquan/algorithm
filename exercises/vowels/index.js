d6008d36a85f5579e7af8c28b9413d411d6568af
Nick Quan <nickkquan@gmail.com>
2018-05-22 11:27:00 -0700

    added optimized solutions for vowels

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,7 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
+// Iterative solution not using includes
 function vowels(str) {
     const revisedString = cleanStr(str).split("");
     const vowel = ['a', 'e', 'i', 'o', 'u'];
@@ -24,6 +25,26 @@ function vowels(str) {
 
 function cleanStr(str) {
     return str.replace(/[^\w]/g, "").toLowerCase()
+}
+
+
+// Iterative solution using includes method
+function vowels(str){
+    let count = 0;
+    const vowelArray = ['a','e','i','o','u'];
+    for (let char of str.toLowerCase()){
+        if (vowelArray.includes(char)){
+            count++
+        }
+    }
+    return count;
+}
+
+// Solution using regular expression
+function vowels(str){
+    const matches = str.match(/[aeiou]/gi);
+
+    return matches ? matches.length : 0;
 }
 
 module.exports = vowels;
